b451c7822f687ec0c9e9766b427a3119ef672de7
matheusDev <matheus.info29@gmail.com>
2024-04-08 22:17:24 -0300

    Update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,18 +1,24 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    keyboard:true,
+var swiper = new Swiper(".swiper", {
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // Optional parameters
+  loop: true,
 
-  });
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  keyboard: true,
+});
